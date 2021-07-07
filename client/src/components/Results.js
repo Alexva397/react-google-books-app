a344917382d19eef97e8926f8c8c6922c44e7f3d
Alexander Vadeboncoeur <asvadeboncoeur@gmail.com>
2021-07-06 21:54:24 -0500

    added map function over authors array in results component, moved material-ui imports to one line

--- client/src/components/Results.js
+++ client/src/components/Results.js
@@ -1,12 +1,5 @@
-
 import { makeStyles } from '@material-ui/core/styles';
-import { Grid } from "@material-ui/core";
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import CardMedia from '@material-ui/core/CardMedia';
+import { Grid, Card, CardActions, CardContent, Button, Typography, CardMedia } from "@material-ui/core";
 
 const useStyles = makeStyles({
     root: {
@@ -49,7 +42,9 @@ function Results({ results, handleBookSave }) {
                                                 {book.volumeInfo.title}
                                             </Typography>
                                             <Typography variant="subtitle1" color="textSecondary">
-                                                {book.volumeInfo.authors}
+                                                {book.volumeInfo.authors === undefined ? "No listed authors." : book.volumeInfo.authors.map((author, index) => {
+                                                    return <span key={index}> {author}{index < book.volumeInfo.authors.length - 1 ? "," : ""}</span>;
+                                                })}
                                             </Typography>
                                         </CardContent>
                                         <CardActions>
